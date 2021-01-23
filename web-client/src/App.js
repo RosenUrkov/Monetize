5fe7c2e8db7740ba1df852eb192c181526162e8f
RosenUrkov <rosen.urkov@gmail.com>
2021-01-23 18:04:59 +0200

    Adde layout hoc

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,13 +1,13 @@
 import { useEffect } from "react";
 import { Provider, useDispatch, useSelector } from "react-redux";
 import { BrowserRouter, Switch, Redirect, Route } from "react-router-dom";
-import Navigation from "./components/Navigation/Navigation";
 import NotFound from "./components/Error/NotFound/NotFound";
 import Login from "./containers/Authentication/Login/Login";
 import Register from "./containers/Authentication/Register/Register";
 import Balance from "./containers/Balance/Balance";
 import Budget from "./containers/Budget/Budget";
 import { authCheckState } from "./store/actions/auth";
+import Layout from "./hoc/Layout";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -18,19 +18,19 @@ const App = () => {
 
   return (
     <BrowserRouter>
-      <Navigation />
+      <Layout>
+        <Switch>
+          <Redirect from="/" to="/register" exact />
 
-      <Switch>
-        <Redirect from="/" to="/register" exact />
+          <Route path="/balance" component={Balance} />
+          <Route path="/budgets" component={Budget} />
 
-        <Route path="/balance" component={Balance} />
-        <Route path="/budgets" component={Budget} />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
 
-        <Route path="/register" component={Register} />
-        <Route path="/login" component={Login} />
-
-        <Route path="*" component={NotFound} />
-      </Switch>
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Layout>
     </BrowserRouter>
   );
 };
