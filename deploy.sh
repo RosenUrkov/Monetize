a04ed94719c4dbc45faa0b873690da9a78653069
Rosen Urkov <rosen.urkov@gmail.com>
2021-02-10 18:23:48 +0200

    Added travis setup for deployment to google cloud

--- deploy.sh
+++ deploy.sh
@@ -15,3 +15,11 @@ docker push rosenurkov/monetize-api-gateway:$SHA
 docker push rosenurkov/monetize-balance-service:$SHA
 docker push rosenurkov/monetize-budget-service:$SHA
 docker push rosenurkov/monetize-statistics-service:$SHA
+
+kubectl apply -f ./k8s/
+
+kubecl set image deployments/web-client-deployment web-client=rosenurkov/monetize-web-client:$SHA
+kubecl set image deployments/api-gateway-deployment api-gateway=rosenurkov/monetize-api-gateway:$SHA
+kubecl set image deployments/balance-service-deployment balance-service=rosenurkov/monetize-balance-service:$SHA
+kubecl set image deployments/budget-service-deployment budget-service=rosenurkov/monetize-budget-service:$SHA
+kubecl set image deployments/statistics-service-deployment statistics-service=rosenurkov/monetize-statistics-service:$SHA
