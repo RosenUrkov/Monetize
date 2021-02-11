189ac9e2f2627dcd66c8658d62651d28ac30301a
Rosen Urkov <rosen.urkov@gmail.com>
2021-02-11 14:25:52 +0200

    Fixed react env variable name

--- web-client/src/components/Error/NotFound/NotFound.js
+++ web-client/src/components/Error/NotFound/NotFound.js
@@ -5,18 +5,18 @@ const NotFound = (props) => {
   return (
     <div className="NotFound">
       <div id="clouds">
-        <div class="cloud x1"></div>
-        <div class="cloud x1_5"></div>
-        <div class="cloud x2"></div>
-        <div class="cloud x3"></div>
-        <div class="cloud x4"></div>
-        <div class="cloud x5"></div>
+        <div className="cloud x1"></div>
+        <div className="cloud x1_5"></div>
+        <div className="cloud x2"></div>
+        <div className="cloud x3"></div>
+        <div className="cloud x4"></div>
+        <div className="cloud x5"></div>
       </div>
-      <div class="c">
-        <div class="_404">404</div>
+      <div className="c">
+        <div className="_404">404</div>
         <hr />
-        <div class="_1">THE PAGE</div>
-        <div class="_2">WAS NOT FOUND</div>
+        <div className="_1">THE PAGE</div>
+        <div className="_2">WAS NOT FOUND</div>
       </div>
     </div>
   );
